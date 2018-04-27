8c32b6b9f3b019c987e299a33b8d63a4fe54d8af
liuzhiyong <1351493417@qq.com>
2018-04-28 00:59:46 +0800

    第一次啊

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div id="app">
+    <inhead></inhead>
+    <Tab></Tab>
+    <keep-alive>
+    	<router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+	import inhead from 'components/inhead/inhead'
+	import Tab from 'components/tab/tab'
+	export default {
+	  components: {
+	  	inhead,
+	  	Tab
+	  }
+	}
+
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,62 @@
+<template>
+  <div ref="wrapper">
+	<slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+	import BScorll from 'better-scroll'
+	export default {
+		props: {
+			probeType: {
+				type: Number,
+				default: 1
+			},
+			click: {
+				type: Boolean,
+				default: true
+			},
+			data: {
+				type: Array,
+				default: null
+			}
+		},
+		mounted(){
+			setTimeout(() => {
+				this._initScroll()
+			})
+		}
+		methods: {
+			_initScroll() {
+				if(!this.$refs.wrapper) {
+					return
+				}
+				this.scorll = new BScorll(this.$refs.wrapper, {
+					probeType: this.probeType,
+					click: this.click
+				})
+			},
+			enabel() {
+				this.scroll && this.ccroll.enabel()
+			},
+			disable() {
+				this.scroll && this.scroll.disable()
+			},
+			refresh() {
+				this.scroll && this.scroll.refresh()
+			}
+		},
+		watch() {
+			data() {
+				setTimeout( () => {
+					this.refresh()
+				}, 20 )
+			}
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+
+</style>
